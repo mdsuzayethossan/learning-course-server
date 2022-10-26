1171ac7fe05bb3c05c8b195e5cb185f66c2df853
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-10-26 09:26:14 +0600

    change

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get("/skill-categories", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((n) => n._id === id);
+  res.send(selectedCourse);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
